0f41c97d76bff282b7035316aa9b36932f9548ac
geomin76 <geom@vt.edu>
2021-08-21 12:47:52 -0400

    Adding random function

--- pages/result.tsx
+++ pages/result.tsx
@@ -6,6 +6,7 @@ import { RequestDocument } from 'graphql-request/dist/types'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { Button } from '@material-ui/core'
 // const fetcher = (query: RequestDocument) => request('/api/graphql', query)
 
 export async function getServerSideProps(context: { query: { lat: any; lng: any } }) {
@@ -21,25 +22,57 @@ export async function getServerSideProps(context: { query: { lat: any; lng: any 
 
 const Result: NextPage = (props) => {
 
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState<number | null>(null);
 
   useEffect(() => {
     if (props.lat && props.lng) {
-      const url = `http://localhost:5000/restaurants?lat=${props.lat}&lng=${props.lng}`
-      fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setData(data.data.search.business);
-      })
+      fetchData();
     }
   }, [])
+
+  useEffect(() => {
+    if (data.length > 0 && selected === null) {
+      const randomElement = Math.floor(Math.random() * data.length)
+      setSelected(randomElement);
+    }
+  }, [data, selected])
+
+  const fetchData = () => {
+    const url = `http://localhost:5000/restaurants?lat=${props.lat}&lng=${props.lng}`
+    fetch(url)
+    .then(response => response.json())
+    .then(incomingData => {
+      setData(incomingData.data.search.business);
+    })
+  }
+
+  const randomSelect = (index: number) => {
+    console.log(`before ${data.length}`);
+    const temp = data;
+    temp.splice(index, 1);
+    setData(temp);
+    console.log(`After ${data.length}`);
+    if (data.length === 0) {
+      fetchData();
+    }
+    const randomElement = Math.floor(Math.random() * data.length)
+    setSelected(randomElement);
+  }
 
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}>
         <h1>First Post</h1>
+        <h2>{selected && data[selected].name}</h2>
+        { selected && 
+          <Button variant="contained" color="primary" onClick={() => {
+            randomSelect(selected);
+          }}>
+            Another random restaurant
+          </Button>
+        } 
         <h2>
           <Link href="/">
             <a>Back to home</a>
