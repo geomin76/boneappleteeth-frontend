1d107174eeaedc21f4147e6b3fd6b3ba4c483ff7
Geo Min <31418725+geomin76@users.noreply.github.com>
2021-08-21 13:17:26 -0400

    Random (#3)
    * Adding random function
    
    * Adding async
    
    * Adding more async
    
    * Random restaurant works

--- pages/result.tsx
+++ pages/result.tsx
@@ -6,6 +6,7 @@ import { RequestDocument } from 'graphql-request/dist/types'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { Button } from '@material-ui/core'
 // const fetcher = (query: RequestDocument) => request('/api/graphql', query)
 
 export async function getServerSideProps(context: { query: { lat: any; lng: any } }) {
@@ -21,25 +22,65 @@ export async function getServerSideProps(context: { query: { lat: any; lng: any 
 
 const Result: NextPage = (props) => {
 
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState<number | null>(null);
 
   useEffect(() => {
     if (props.lat && props.lng) {
-      const url = `http://localhost:5000/restaurants?lat=${props.lat}&lng=${props.lng}`
-      fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setData(data.data.search.business);
-      })
+      fetchData();
     }
   }, [])
+
+  useEffect(() => {
+    if (data.length > 0 && selected === null) {
+      console.log("first and should be only")
+      const randomElement = Math.floor(Math.random() * data.length)
+      setSelected(randomElement);
+    }
+  }, [data])
+
+  const fetchData = () => {
+    const url = `http://localhost:5000/restaurants?lat=${props.lat}&lng=${props.lng}`
+    fetch(url)
+    .then(response => response.json())
+    .then(incomingData => {
+      setData(incomingData.data.search.business);
+    })
+  }
+
+  const settingData = async (temp: any) => {
+    setData(temp);
+  }
+
+  const settingSelected = async (val: number) => {
+    setSelected(val);
+  }
+
+  const randomSelect = async (index: number) => {
+    const temp = data;
+    temp.splice(index, 1);
+    settingData(temp);
+    if (data.length === 0) {
+      fetchData();
+      return;
+    }
+    const randomElement = Math.floor(Math.random() * data.length)
+    settingSelected(randomElement);
+  }
 
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}>
         <h1>First Post</h1>
+        <h2>{selected !== null && data[selected].name}</h2>
+        { selected !== null && 
+          <Button variant="contained" color="primary" onClick={() => {
+            randomSelect(selected);
+          }}>
+            Another random restaurant
+          </Button>
+        } 
         <h2>
           <Link href="/">
             <a>Back to home</a>
